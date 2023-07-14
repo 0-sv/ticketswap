6bc58632b6b1c39235b86ef9fc723c7acf26197e
A.J.S. Verbeek <ajsverbeek@gmail.com>
2023-07-14 12:20:01 +0200

    feat: add libs

---
+++ README.md
@@ -0,0 +1,3 @@
+```
+sudo apt install cmake clang build-essential gdb libxml2-dev libssl-dev
+```
--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <libxml/parser.h>
+#include <libxml/tree.h>
+#include <cpr/cpr.h>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
